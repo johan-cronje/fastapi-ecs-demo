c72d4af907eeede819e23d94bb914c86c55d9a61
Johan Cronje <jcronje@hotmail.com>
2023-12-12 14:08:55 -0800

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "success new"}
+
+if __name__ == '__main__':
+    uvicorn.run("0.0.0.0", port=8081, reload=True, access_log=False)
